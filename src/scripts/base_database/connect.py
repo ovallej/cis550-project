d221f74a3085243dc63f5de75e178e589d477b6c
ovallej <ovallej@seas.upenn.edu>
2016-11-14 11:19:02 -0500

    added partly hometowns

--- src/scripts/base_database/connect.py
+++ src/scripts/base_database/connect.py
@@ -2,13 +2,17 @@ import cx_Oracle
 username = 'cis550project'
 password = 'Obstacle123'
 
-host = 'cis550project.cc4zli6er8qm.us-west-2.rds.amazonaws.com'
+cis550project.cksgc6f8zhlj.us-east-1.rds.amazonaws.com:1521
+#host = 'cis550project.cc4zli6er8qm.us-west-2.rds.amazonaws.com'
+host = 'cis550project.cksgc6f8zhlj.us-east-1.rds.amazonaws.com'
 port = '1521'
 sid = 'ORCL'
 
 dsn = cx_Oracle.makedsn(host, port, sid)
 db = cx_Oracle.connect(username, password,dsn)
 c = db.cursor()
-c.execute('select * from Person')
+#c.execute('select * from Person')
 val = c.execute('select * from Person')
 val.fetchall()
+
+sqlplus 'cis550project@(DESCRIPTION=(ADDRESS=(PROTOCOL=TCP)(HOST=cis550project.cksgc6f8zhlj.us-east-1.rds.amazonaws.com)(PORT=1521))(CONNECT_DATA=(SID=ORCL)))'
